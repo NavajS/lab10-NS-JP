059c6972606c65360eb416991cdac7f369fb5032
NavajS <spnavaj@gmail.com>
2025-04-09 14:01:40 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -4,9 +4,7 @@ calculator.py
 
 One function per operation, in order.
 """
-<<<<<<< HEAD
-# First example
-=======
+
 import math
 
 def square_root(a):
@@ -20,9 +18,6 @@ def hypotenuse(a, b):
 
 def add(a, b):
     a + b
-
-def subtract(a, b):
-    a - b
 
 def multiply(a, b):
     a * b
@@ -42,31 +37,3 @@ def logarithm(a, b):
 
 def exponent(a, b):
     a**b
-
->>>>>>> 7dedaea0adbe3bea988e751e63348f1ff3303a50
-
-import math
-
-def add(a, b):
-    a + b
-
-def sub(a, b):
-    a - b
-
-def mul(a, b):
-    a * b
-
-def div(a, b):
-    try:
-        b / a
-    except ZeroDivisionError as e:
-        print(e)
-
-def log(a, b):
-    try:
-        math.log(b) / math.log(a)
-    except ValueError as e:
-        print(e)
-
-def exp(a, b):
-    a ** b
